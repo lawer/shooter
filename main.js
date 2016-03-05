85fa16bca8c02842a834fb0b1be4cc5c87bd117e
lawer <carlesgm@gmail.com>
2016-03-05 22:50:50 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,38 @@
+/// <reference path="phaser/phaser.d.ts"/>
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+var mainState = (function (_super) {
+    __extends(mainState, _super);
+    function mainState() {
+        _super.apply(this, arguments);
+    }
+    mainState.prototype.preload = function () {
+        _super.prototype.preload.call(this);
+        this.load.image('background', 'assets/sea.png');
+        this.load.image('bullet', 'assets/bullet.png');
+    };
+    mainState.prototype.create = function () {
+        _super.prototype.create.call(this);
+        this.background = this.add.tileSprite(0, 0, 800, 600, 'background');
+        this.bullet = this.add.sprite(400, 300, 'bullet');
+    };
+    mainState.prototype.update = function () {
+        _super.prototype.update.call(this);
+    };
+    return mainState;
+})(Phaser.State);
+var SimpleGame = (function () {
+    function SimpleGame() {
+        this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'gameDiv');
+        this.game.state.add('main', mainState);
+        this.game.state.start('main');
+    }
+    return SimpleGame;
+})();
+window.onload = function () {
+    var game = new SimpleGame();
+};
+//# sourceMappingURL=main.js.map
---
+++ main.ts
@@ -0,0 +1,43 @@
+/// <reference path="phaser/phaser.d.ts"/>
+
+class mainState extends Phaser.State {
+    game: Phaser.Game;
+    private background:Phaser.TileSpriteSprite;
+    private bullet:Phaser.Sprite;
+
+    private cursor:Phaser.CursorKeys;
+
+    preload():void {
+        super.preload();
+
+        this.load.image('background', 'assets/sea.png');
+        this.load.image('bullet', 'assets/bullet.png');
+    }
+
+    create():void {
+        super.create();
+
+        this.background = this.add.tileSprite(0, 0, 800,  600, 'background');
+        this.bullet = this.add.sprite(400, 300, 'bullet');
+
+    }
+
+    update():void {
+        super.update();
+    }
+}
+
+class SimpleGame {
+    game:Phaser.Game;
+
+    constructor() {
+        this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'gameDiv');
+
+        this.game.state.add('main', mainState);
+        this.game.state.start('main');
+    }
+}
+
+window.onload = () => {
+    var game = new SimpleGame();
+};
