78a4fdc1031e1df0cc59c3224105c73b99615b20
lawer <carlesgm@gmail.com>
2016-03-05 22:50:50 +0100

    Initial commit

---
+++ main.ts
@@ -0,0 +1,39 @@
+/// <reference path="phaser/phaser.d.ts"/>
+
+class mainState extends Phaser.State {
+    game: Phaser.Game;
+    private ufo:Phaser.Sprite;
+    private cursor:Phaser.CursorKeys;
+
+    preload():void {
+        super.preload();
+
+        this.load.image('background', 'assets/sea.png');
+    }
+
+    create():void {
+        super.create();
+        var background;
+
+        background = this.add.tileSprite(0, 0, 800,  600, 'background');
+    }
+
+    update():void {
+        super.update();
+    }
+}
+
+class SimpleGame {
+    game:Phaser.Game;
+
+    constructor() {
+        this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'gameDiv');
+
+        this.game.state.add('main', mainState);
+        this.game.state.start('main');
+    }
+}
+
+window.onload = () => {
+    var game = new SimpleGame();
+};
